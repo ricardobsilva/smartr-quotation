b8e8ad29091bda1c678737ecb9161a9fcf790524
Ricardo Silva <ricardo@safetysystemtechnology.com.br>
2019-02-06 23:04:28 -0300

    Create Connection and Request classes to comunicate with Exchangerate API

---
+++ lib/exchangerate/request.rb
@@ -0,0 +1,12 @@
+class Exchangerate::Request
+  class << self
+    def where(resource_path)
+      response = api.get(resource_path)
+      JSON.parse(response.body)
+    end
+
+    def api
+      Exchangerate::Connection.api
+    end
+  end
+end
